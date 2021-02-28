56a348fe9f981bde0b8fc6ffddd500d56ff6b674
Shobhit2393 <79789139+Shobhit2393@users.noreply.github.com>
2021-02-28 21:09:27 +0530

    changes done

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { User } from 'src/app/model/user.model';
+
 
 @Component({
   selector: 'app-login',
@@ -10,13 +12,34 @@ import { User } from 'src/app/model/user.model';
 export class LoginComponent implements OnInit {
   ErrorMsg:string='';
   User: User = new User();
-  constructor(private router: Router) { }
+  constructor(private router: Router,private messageService: MessageService) { }
 
   ngOnInit(): void {
   }
   login() {
+    debugger;
+    if(this.User.userName.trim()==null || this.User.userName.trim()=="")
+    {
+      this.ErrorMsg="UserName is required";
+      return;
+    }
+    if(this.User.password.trim()==null || this.User.password.trim()=="")
+    {
+      this.ErrorMsg="Password is required";
+      return;
+    }
     if (this.User.userName=='blogger@grapecity.com' && this.User.password=='1qaz!QAZ') {
       this.router.navigate(['home/allrecord']);
     }
+    else
+    {
+      this.ErrorMsg="Incorrect credentials";
+      return;
+    }
   }
+    onChange()
+    {
+      debugger;
+      this.ErrorMsg=""; 
+    }
 }
